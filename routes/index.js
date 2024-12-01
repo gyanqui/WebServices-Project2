1b2fd40a757e7560357bf5d2b5337242a824662e
gyanqui <gyanqui3@gmail.com>
2024-12-01 01:24:46 -0500

    Add Sellers Collections and OAUTH

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ const passport = require('passport');
 const router = require('express').Router();
 
 router.use('/', require('./swagger'));
-router.get('/', (req, res) => { res.send('Welcome to my API Application');});
+router.get('/', (req, res) => { 
+    res.send((req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : `Logged out`));
+});
+
 router.use('/products', require('./products'));
 router.use('/sellers', require('./sellers'));
 
--- server.js
+++ server.js
@@ -22,11 +22,12 @@ app
 
     .use(passport.session())
 
-    .use((req, res, next) => {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 
-            'POST, GET, PUT, PATCH, DELETE, OPTIONS'
-        );
+    use((req,res,next)=>{
+        res.setHeader("Access-Control-Allow-Origin", "*",);
+        res.setHeader("Access-Control-Allow-Headers", 
+            "Origin, X-requested-With, Content-Type, Accept, Z-Key, Authorization");
+        res.setHeader("Access-Control-Allow-Methods", 
+            "POST, GET, PUT, PATCH, OPTIONS DELETE");
         next();
     })
     .use(cors({ methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']}))
@@ -53,10 +54,8 @@ passport.deserializeUser((user, done) => {
     done(null, user);
 });
 
-app.get('/', (req, res) => { res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : "Logged Out")});
-
 app.get('/github/callback', passport.authenticate('github', {
-    failureRedirect: '/api-docs', session:false}),
+    failureRedirect: '/api-docs', session: false}),
     (req, res) => {
         req.session.user = req.user;
         res.redirect('/');
